51ca040637b76a280028e7705ee737fb4b22a297
panjiabin <panjiabin@shanghai-fanuc.com.cn>
2023-01-13 15:15:42 +0800

    MaskRCNN pytorch

---
+++ src/onnx_model_export.py
@@ -0,0 +1,147 @@
+import os
+import torch
+import onnx
+import onnxruntime
+from onnxsim import simplify
+from PIL import Image
+from torchvision import transforms
+from backbone import resnet50_fpn_backbone
+from network_files import MaskRCNN
+
+
+# project root dir and src root dir
+ROOT_DIR = os.path.abspath("../")
+SRC_ROOT_DIR = os.path.abspath("./")
+
+# output model dir(include epoch models and frozen model)
+MODELS_DIR = os.path.join(ROOT_DIR, "models")
+
+# transform pytorch model path
+WEIGHT_PATH = os.path.join(MODELS_DIR, "epoch_models", "epoch_model_35.pth")
+
+# export onnx model dir
+ONNX_MODEL_DIR = os.path.join(MODELS_DIR, "onnx_models")
+
+# test model images path
+IMG_PATH = os.path.join(ROOT_DIR, "coco_dataset", "images", "1_color.png")
+
+
+############################################################
+# utils
+############################################################
+
+def create_model(num_classes, box_thresh=0.5):
+    backbone = resnet50_fpn_backbone()
+    model = MaskRCNN(backbone,
+                     num_classes=num_classes,
+                     rpn_score_thresh=box_thresh,
+                     box_score_thresh=box_thresh)
+    return model
+
+
+def export_model_from_pytorch_to_onnx(pytorch_model, device, img_file, onnx_model_name):
+    # input of the model (from pil image to tensor, do not normalize image)
+    original_img = Image.open(img_file).convert('RGB')      # load image
+    data_transform = transforms.Compose([transforms.ToTensor()])
+    img = data_transform(original_img)
+    img = torch.unsqueeze(img, dim=0).to(device)        # expand batch dimension to device
+
+    pytorch_model.eval()
+    # test model input
+    im = torch.randn(1, 3, 256, 256)
+    out = pytorch_model(im)
+    print("out:", out)
+
+    # export the model
+    dy_axes = {'input': {0: 'batch', 2: 'height', 3: 'width'}}
+    input_label = ['in_img']
+    output_label = ['out_box', 'out_accept', 'out_score', 'out_mask']
+    torch.onnx.export(pytorch_model,                # model being run
+                      img,                          # model input (or a tuple for multiple inputs)
+                      onnx_model_name,              # where to save the model (can be a file or file-like object)
+                      export_params=True,           # store the trained parameter weights inside the model file
+                      opset_version=11,             # the ONNX version to export the model to
+                      do_constant_folding=True,     # whether to execute constant folding for optimization
+                      input_names=input_label,      # the model's input names
+                      output_names=output_label,    # the model's output names
+                      dynamic_axes=None)              # variable length axes
+
+
+def verify_onnx_model(onnx_model_name, img_file):
+    # model is an in-memory ModelProto
+    model = onnx.load(onnx_model_name)
+    # print("the model is:\n{}".format(model))
+
+    # check the model
+    try:
+        onnx.checker.check_model(model)
+    except onnx.checker.ValidationError as e:
+        print("  the model is invalid: %s" % e)
+        exit(1)
+    else:
+        print("  the model is valid")
+
+    # verify onnx model inference
+    ort_session = onnxruntime.InferenceSession(onnx_model_name)
+    original_img = Image.open(img_file).convert('RGB')
+    data_transform = transforms.Compose([transforms.ToTensor()])
+    img = data_transform(original_img)
+    input_img = torch.unsqueeze(img, dim=0).numpy()         # input_img: onnx model input image data
+    ort_inputs = {'in_img': input_img}                       # define input dictionary
+    try:
+        ort_box = ort_session.run(['out_box'], ort_inputs)[0]     # onnx model inference
+        ort_score = ort_session.run(['out_accept'], ort_inputs)[0]
+        ort_mask = ort_session.run(['out_mask'], ort_inputs)[0]
+        print("Onnx model inference result:\n{}".format(ort_mask))
+    except:
+        print("Onnx model inference fail.")
+    # else:
+    #     ort_output = np.squeeze(ort_output, 0)
+    #     ort_output = np.clip(ort_output, 0, 255)
+    #     ort_output = np.transpose(ort_output, [1, 2, 0]).astype(np.uint8)
+    #     cv2.imshow("MaskRCNN Detection Demo", ort_output)
+
+
+############################################################
+# main function
+############################################################
+
+def main():
+    # parameters initial
+    num_classes = 2     # background + sack
+
+    # get devices
+    device = torch.device("cpu")    # export onnx model with cpu
+    print("using {} device.".format(device))
+
+    # create model & load train weights
+    model = create_model(num_classes=num_classes)
+
+    assert os.path.exists(WEIGHT_PATH), "{} file dose not exist.".format(WEIGHT_PATH)
+    weights_dict = torch.load(WEIGHT_PATH, map_location='cpu')
+    weights_dict = weights_dict["model"] if "model" in weights_dict else weights_dict
+    model.load_state_dict(weights_dict)
+    model.to(device)
+
+    # check onnx model dir
+    if not os.path.exists(ONNX_MODEL_DIR):
+        os.makedirs(ONNX_MODEL_DIR)
+
+    # export onnx model
+    onnx_model_path = os.path.join(ONNX_MODEL_DIR, "mask_rcnn.onnx")
+    export_model_from_pytorch_to_onnx(model, device, IMG_PATH, onnx_model_path)     # export onnx model
+
+    # simplify export onnx model
+    onnx_sim_model_path = os.path.join(ONNX_MODEL_DIR, 'mask_rcnn_sim.onnx')
+    onnx_model = onnx.load(onnx_model_path)
+    onnx_sim_model, check = simplify(onnx_model)        # simplify onnx model
+    assert check, "Simplified ONNX model could not be validated"
+    onnx.save(onnx_sim_model, onnx_sim_model_path)
+    print('ONNX file simplified!')
+
+    # check export onnx model
+    verify_onnx_model(onnx_sim_model_path, IMG_PATH)    # verify onnx model
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/train.py
@@ -0,0 +1,222 @@
+import os
+import datetime
+
+import torch
+
+from src.utils import transforms
+from network_files import MaskRCNN
+from backbone import resnet50_fpn_backbone
+from torchvision.ops.misc import FrozenBatchNorm2d
+from src.utils.my_dataset_coco import CocoDetection
+from utils import train_eval_utils as utils
+
+############################################################
+#  Configurations
+############################################################
+
+# project root dir and src root dir
+ROOT_DIR = os.path.abspath("../")
+SRC_ROOT_DIR = os.path.abspath("./")
+
+# train record file dir
+LOG_DIR = os.path.join(ROOT_DIR, "log")
+
+# coco dataset root dir(include annotations and images)
+DATA_ROOT_DIR = os.path.join(ROOT_DIR, "coco_dataset")
+
+# output model dir(include epoch models and frozen model)
+MODELS_DIR = os.path.join(ROOT_DIR, "models")
+
+# train with coco weight or not
+TRAIN_WITH_COCO = True
+
+# COCO_MODEL_DIR include model file and coco weight
+COCO_MODEL_DIR = os.path.join(SRC_ROOT_DIR, "models")
+
+# if not train with coco weight, this need to be defined
+# BASE_WEIGHT_PATH -> path/to/last/trained/model/weight
+BASE_WEIGHT_PATH = []
+
+
+# train configuration
+class TrainConfig():
+    # Train epochs
+    EPOCHS = 36
+
+    # Batch_size
+    BatchSize = 2
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # Background + sack
+
+    # Number of workers
+    # NUM_WORKERS = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])
+    NUM_WORKERS = 1
+
+    # Learning rate and momentum
+    # The Mask RCNN paper uses lr=0.02, but on TensorFlow it causes
+    # weights to explode. Likely due to differences in optimizer
+    # implementation.
+    LEARNING_RATE = 0.004
+    LEARNING_MOMENTUM = 0.9
+
+    # Weight decay regularization
+    WEIGHT_DECAY = 0.0001
+
+
+############################################################
+# utils
+############################################################
+
+def create_model(num_classes, load_pretrain_weights=True):
+    # 如果GPU显存很小，batch_size不能设置很大，建议将norm_layer设置成FrozenBatchNorm2d(默认是nn.BatchNorm2d)
+    # FrozenBatchNorm2d的功能与BatchNorm2d类似，但参数无法更新
+    # trainable_layers include ['layer4', 'layer3', 'layer2', 'layer1', 'conv1']，
+    # trainable_layers=5: train all layers
+    backbone = resnet50_fpn_backbone(norm_layer=FrozenBatchNorm2d,
+                                     trainable_layers=5)
+    # resnet50 imagenet weights url: https://download.pytorch.org/models/resnet50-0676ba61.pth
+    # backbone = resnet50_fpn_backbone(pretrain_path="./model/resnet50.pth", trainable_layers=3)
+    model = MaskRCNN(backbone, num_classes=num_classes)
+
+    if load_pretrain_weights:
+        # coco weights url: "https://download.pytorch.org/models/maskrcnn_resnet50_fpn_coco-bf2d0c1e.pth"
+        weights_dict = torch.load("./model/maskrcnn_resnet50_fpn_coco.pth", map_location="cpu")
+        for k in list(weights_dict.keys()):
+            if ("box_predictor" in k) or ("mask_fcn_logits" in k):
+                del weights_dict[k]
+        print(model.load_state_dict(weights_dict, strict=False))
+    return model
+
+
+############################################################
+# main function
+############################################################
+
+def main():
+
+    # get train config
+    config = TrainConfig()
+
+    # train on the GPU or on the CPU, if a GPU is not available
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    print("Using {} device training.".format(device.type))
+
+    # save train info file to det_results_file & seg_results_file
+    now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    if not os.path.exists(LOG_DIR):
+        os.makedirs(LOG_DIR)
+    det_results_file = LOG_DIR+f"/det_results{now}.txt"
+    seg_results_file = LOG_DIR+f"/seg_results{now}.txt"
+
+    # define model input data
+    data_transform = {
+        "train": transforms.Compose([transforms.ToTensor(),
+                                     transforms.RandomHorizontalFlip(0.5)]),
+        "val": transforms.Compose([transforms.ToTensor()])
+    }
+
+    # load train dataset
+    train_dataset = CocoDetection(DATA_ROOT_DIR, "train", data_transform["train"])
+    print('Using %g dataloader workers' % config.NUM_WORKERS)
+
+    train_data_loader = torch.utils.data.DataLoader(train_dataset,
+                                                    batch_size=config.BatchSize,
+                                                    shuffle=True,
+                                                    pin_memory=True,
+                                                    num_workers=config.NUM_WORKERS,
+                                                    collate_fn=train_dataset.collate_fn)
+
+    # load validation data set
+    val_dataset = CocoDetection(DATA_ROOT_DIR, "val", data_transform["val"])
+    val_data_loader = torch.utils.data.DataLoader(val_dataset,
+                                                  batch_size=1,
+                                                  shuffle=False,
+                                                  pin_memory=True,
+                                                  num_workers=config.NUM_WORKERS,
+                                                  collate_fn=train_dataset.collate_fn)
+
+    # create model num_classes
+    model = create_model(num_classes=config.NUM_CLASSES,
+                         load_pretrain_weights=TRAIN_WITH_COCO)
+    model.to(device)
+
+    # define optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params,
+                                lr=config.LEARNING_RATE,
+                                momentum=config.LEARNING_MOMENTUM,
+                                weight_decay=config.WEIGHT_DECAY)
+
+    # learning rate scheduler
+    lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+        optimizer, milestones=[16, 22], gamma=0.1)
+
+    train_loss = []
+    learning_rate = []
+    val_map = []
+    scaler = torch.cuda.amp.GradScaler()
+    # if not train with coco weight，train based on BASE_WEIGHT_PATH
+    if not TRAIN_WITH_COCO:
+        # If map_location is missing, torch.load will first load the module to CPU
+        # and then copy each parameter to where it was saved,
+        # which would result in all processes on the same machine using the same set of devices.
+        checkpoint = torch.load(BASE_WEIGHT_PATH, map_location=device)
+        model.load_state_dict(checkpoint['model'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
+        if "scaler" in checkpoint:
+            scaler.load_state_dict(checkpoint["scaler"])
+
+    for epoch in range(0, config.EPOCHS):
+        # train for one epoch, printing every 50 iterations
+        mean_loss, lr = utils.train_one_epoch(model, optimizer, train_data_loader,
+                                              device, epoch, print_freq=10,
+                                              warmup=True, scaler=scaler)
+        train_loss.append(mean_loss.item())
+        learning_rate.append(lr)
+
+        # update the learning rate
+        lr_scheduler.step()
+
+        # evaluate on the test dataset
+        det_info, seg_info = utils.evaluate(model, val_data_loader, device=device)
+
+        # write detection into txt (coco id, loss, learning rate)
+        with open(det_results_file, "a") as f:
+            result_info = [f"{i:.4f}" for i in det_info + [mean_loss.item()]] + [f"{lr:.6f}"]
+            txt = "epoch:{} {}".format(epoch, '  '.join(result_info))
+            f.write(txt + "\n")
+
+        # write seg into txt (coco id, loss, learning rate)
+        with open(seg_results_file, "a") as f:
+            result_info = [f"{i:.4f}" for i in seg_info + [mean_loss.item()]] + [f"{lr:.6f}"]
+            txt = "epoch:{} {}".format(epoch, '  '.join(result_info))
+            f.write(txt + "\n")
+
+        val_map.append(det_info[1])         # pascal mAP
+
+        # save epoch model weights
+        epoch_dir = os.path.join(MODELS_DIR, "epoch_models")
+        if not os.path.exists(epoch_dir):
+            os.makedirs(epoch_dir)
+        save_files = {
+            'model': model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'lr_scheduler': lr_scheduler.state_dict(),
+            'epoch': epoch}
+        torch.save(save_files, epoch_dir+"/epoch_model_{}.pth".format(epoch))
+
+    # plot loss and lr (learn rate) curve
+    if len(train_loss) != 0 and len(learning_rate) != 0:
+        from src.utils.plot_curve import plot_loss_and_lr
+        plot_loss_and_lr(train_loss, learning_rate, LOG_DIR)
+
+    # plot mAP (Mean Average Precision) curve
+    if len(val_map) != 0:
+        from src.utils.plot_curve import plot_map
+        plot_map(val_map, LOG_DIR)
+
+
+if __name__ == "__main__":
+    main()
