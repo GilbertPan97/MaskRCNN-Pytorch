20b689941a9e317d3cb5481a2f5665eb1ddb5a65
panjiabin <panjiabin@shanghai-fanuc.com.cn>
2023-08-18 13:11:59 +0800

    delete opencv 3rd-party

--- torch_script_gen.py
+++ torch_script_gen.py
@@ -5,6 +5,12 @@ from torchvision import transforms
 from backbone import resnet50_fpn_backbone
 from network_files import MaskRCNN
 
+import torchvision.models
+
+from torchvision.ops.misc import FrozenBatchNorm2d
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
 
 # project root dir and src root dir
 ROOT_DIR = os.path.abspath("./")
@@ -35,6 +41,47 @@ def create_model(num_classes, box_thresh=0.5):
     return model
 
 
+def create_model1(num_classes, load_pretrain_weights=True):
+    # 如果GPU显存很小，batch_size不能设置很大，建议将norm_layer设置成FrozenBatchNorm2d(默认是nn.BatchNorm2d)
+    # FrozenBatchNorm2d的功能与BatchNorm2d类似，但参数无法更新
+    # trainable_layers include ['layer4', 'layer3', 'layer2', 'layer1', 'conv1']，
+    # trainable_layers=5: train all layers
+    backbone = resnet50_fpn_backbone(norm_layer=FrozenBatchNorm2d, trainable_layers=5)
+    # resnet50 imagenet weights url: https://download.pytorch.org/models/resnet50-0676ba61.pth
+    # backbone = resnet50_fpn_backbone(pretrain_path="./model/resnet50.pth", trainable_layers=3)
+    model = MaskRCNN(backbone, num_classes=num_classes)
+
+    if load_pretrain_weights:
+        # coco weights url: "https://download.pytorch.org/models/maskrcnn_resnet50_fpn_coco-bf2d0c1e.pth"
+        weights_dict = torch.load("./coco/maskrcnn_resnet50_fpn_coco.pth", map_location="cpu")
+        for k in list(weights_dict.keys()):
+            if ("box_predictor" in k) or ("mask_fcn_logits" in k):
+                del weights_dict[k]
+        print(model.load_state_dict(weights_dict, strict=False))
+    return model
+
+
+def create_model2(num_classes, load_pretrain_weights=True):
+    # get maskrcnn model form torchvision.models
+    # load an instance segmentation model pre-trained on COCO
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=load_pretrain_weights)
+
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # now get the number of input features for the mask classifier
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+    # and replace the mask predictor with a new one
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+                                                       hidden_layer,
+                                                       num_classes)
+
+    return model
+
+
 def generate_torch_script(pytorch_model, device, img_file, save_path):
     # input of the model (from pil image to tensor, do not normalize image)
     original_img = Image.open(img_file).convert('RGB')      # load image
@@ -46,7 +93,7 @@ def generate_torch_script(pytorch_model, device, img_file, save_path):
     pytorch_model.eval()
     if device.type == 'cpu':
         pytorch_model = pytorch_model.cpu()
-    traced_script_module = torch.jit.script(pytorch_model, img)
+    traced_script_module = torch.jit.script(pytorch_model)
     # traced_script_module = torch.jit.trace(pytorch_model, img)
     traced_script_module.save(save_path)
 
@@ -64,7 +111,7 @@ def main():
     print("using {} device.".format(device))
 
     # create model & load train weights
-    model = create_model(num_classes=num_classes)
+    model = create_model2(num_classes=num_classes)
 
     assert os.path.exists(WEIGHT_PATH), "{} file dose not exist.".format(WEIGHT_PATH)
     weights_dict = torch.load(WEIGHT_PATH, map_location='cpu')
@@ -80,9 +127,9 @@ def main():
     data_transform = transforms.Compose([transforms.ToTensor()])
     img = data_transform(original_img)
 
-    # export onnx model
-    model_save_path = os.path.join(MODELS_DIR, "traced_model.pt")
-    generate_torch_script(model, device, IMG_PATH, model_save_path)     # export onnx model
+    # export torch script model
+    model_save_path = os.path.join(MODELS_DIR, "script_model.pt")
+    generate_torch_script(model, device, IMG_PATH, model_save_path)     # export torch script model
     model_script = torch.jit.load(model_save_path)
     model_script.eval()
     with torch.no_grad():
